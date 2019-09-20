03123d222e14e984bcbb295dfc445a735b236277
Ryan Young <young.ryan1979@gmail.com>
2019-09-20 18:24:38 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,5 +13,6 @@ def link (phrase = "It's Dangerous To Go Alone! Take This.")
 end
 
 def any_phrase (phrase)
+  phrase = "Do A Barrell Roll\n"
   puts any_phrase (phrase = "Do A Barrell Roll!\n")
 end
